2fade232ad35f72a00adecb4b7c2e34c9d080148
Jose Urena <urena.jose@gmail.om>
2020-12-06 21:08:15 -0500

    added Twitter.py file, changes to models.py and created base.html

--- src/models.py
+++ src/models.py
@@ -6,11 +6,14 @@ DB = SQLAlchemy()
 class User(DB.Model):
     id = DB.Column(DB.Integer, primary_key=True)
     name = DB.Column(DB.String(30), nullable=False)
+    def __repr__(self):
+        return f'<User: {self.name}>'
 
 
 class Tweet(DB.Model):
     id = DB.Column(DB.Integer, primary_key=True)
     text = DB.Column(DB.Unicode(280), nullable=False)
+    embedding = DB.Column(DB.PickleType, nullable=False)
     user_id = DB.Column(DB.Integer, DB.ForeignKey('user.id'), nullable=False)
     user = DB.relationship('User', backref=DB.backref('tweets', lazy=True))
 
